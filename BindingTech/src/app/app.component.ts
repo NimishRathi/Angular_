72233c56e95f4c4296dd8b74a0443c65c0599b93
NimishRathi <nimish.rathi2002@gmail.com>
2025-03-05 15:59:34 +0530

    [nimish]added

---
+++ BindingTech/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'BindingTech';
+  imgUrl="download.png"
+  ngOnInit(): void {
+    this.title = "Hello from BridgeLabz";
+  }
+}
+
