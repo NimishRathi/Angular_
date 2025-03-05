a5c5e56b25bab7f3e6b4c350a00b6658a3c11873
NimishRathi <nimish.rathi2002@gmail.com>
2025-03-05 14:56:35 +0530

    [nimish]added.UC1_Angular

---
+++ Helloworld/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'Helloworld from Bridge Labz';
+
+ngOnInit(): void{
+  this.title = "hello from BridgeLabz";
+}
+}
